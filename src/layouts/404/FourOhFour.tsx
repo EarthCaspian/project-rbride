931737de49b1e21eb43bf2a84a71f8be5aad749a
EarthCaspian <hazar.a@outlook.com>
2024-01-10 11:43:20 +0300

    404 page

---
+++ src/layouts/404/FourOhFour.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+type Props = {}
+
+const fourOhFour = (props: Props) => {
+  return (
+    <div className="text-center mt-5">
+          <h3>Oops, something went wrong, let's go back.</h3>
+          <Link to="/">Go Back!</Link>
+          </div>
+  )
+}
+
+export default fourOhFour;
