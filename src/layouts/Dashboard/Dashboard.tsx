1618ab3ad553d245f79efca3e6c3b16c8ffa3d57
EarthCaspian <hazar.a@outlook.com>
2024-01-10 11:42:49 +0300

    hero image

---
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h2>Welcome to RoboRide</h2>
+          <p>Rental Services with Precision</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
