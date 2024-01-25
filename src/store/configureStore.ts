2ca3b72498c681fdf3e1438aa83ea19f7982ad8b
senemyilmazz <senemy97@gmail.com>
2024-01-26 02:30:41 +0300

    added combineReducers to combine car and rental reducers

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,15 +1,18 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
 import { cartReducer} from "./cartSlice";
 import { rentalReducer } from "./rentalSlice";
 
 
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  rental: rentalReducer,
+});
+
 const store = () => {
   return configureStore({
-    reducer: {
-      cart: cartReducer,
-      rental: rentalReducer,
-    },
+    reducer: rootReducer,
   });
 }
 
 export default store;
+export type RootState = ReturnType<typeof rootReducer>
