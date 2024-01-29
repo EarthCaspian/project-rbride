27c907a85186f9d0bcc61301b12100094cb52ef8
senemyilmazz <senemy97@gmail.com>
2024-01-30 00:04:09 +0300

    Implemented a function for ensuring type compatibility with the backend's local date format.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-
+//Function to format the date for ensuring type compatibility with the backend's local date format.
 export const formatLocalDateToYYYYMMDD = (date: string) : string => {
     const oldDate = date.split('.');
     const formattedDate = oldDate[2] + "-" + oldDate[1] + "-" + oldDate[0];
