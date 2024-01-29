5fe1d76a203cea0c9cdbcb12ba90f59670eeaad0
senemyilmazz <senemy97@gmail.com>
2024-01-29 23:49:08 +0300

    Implemented add function to send a request to the backend for adding a rental entry to the database.

--- src/services/RentalService.ts
+++ src/services/RentalService.ts
@@ -4,6 +4,7 @@ import { AddRentalRequestModel } from "../models/response/RentalModel";
 const API_URL="http://localhost:8080/rentals";
 
 class RentalService {
+    //Function to send a request to the backend for adding a rental entry to the database.
     add(request : AddRentalRequestModel) {
         axios.post(`${API_URL}/add`, request)
         .then(response => {
