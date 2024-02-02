a9566eb69516cd854c934788c080688ed180c895
EarthCaspian <hazar.a@outlook.com>
2024-02-02 15:35:08 +0300

    conflicts fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { CarDetails } from "./pages/CarDetails/CarDetails";
 import BookNow from "./pages/BookNow/BookNow";
 import AdditionalService from "./pages/AdditionalService/AdditionalService";
 import BookingCompletion from "./pages/BookingCompletion/BookingCompletion";
-
+import LoadingOverlay from "./components/LoadingOverlay/LoadingOverlay";
 import Profile from "./pages/Profile/Profile/Profile";
 import Orders from "./pages/Profile/Order/Orders";
 
