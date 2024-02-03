911aac2f9c2b8db4fc18a033ae2f73d241395bfa
EarthCaspian <hazar.a@outlook.com>
2024-02-03 21:13:29 +0300

    store exports directly, loginReducer added

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -2,19 +2,19 @@ import { combineReducers, configureStore} from "@reduxjs/toolkit";
 import { cartReducer} from "./cartSlice";
 import { rentalReducer } from "./rentalSlice";
 import { loadingReducer } from "./loadingSlice";
+import { loginReducer } from "./loginSlice";
 
 
 const rootReducer = combineReducers({
   cart: cartReducer,
   rental: rentalReducer,
-  loading: loadingReducer
+  loading: loadingReducer,
+  login: loginReducer
 });
 
-const store = () => {
-  return configureStore({
-    reducer: rootReducer,
-  });
-}
+export const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default store;
 export type RootState = ReturnType<typeof rootReducer>
