2e7787ba2ea954438314daf0ae07446fbaca5c49
senemyilmazz <senemy97@gmail.com>
2024-02-13 15:10:23 +0300

    calculateDatesDifference function is moved here

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -5,3 +5,23 @@ export const formatLocalDateToYYYYMMDD = (date: string) : string => {
     return formattedDate;
 }
 
+  // DATES DIFFERENCE CALCULATING
+  // Function to calculate the number of days between two dates
+export const calculateDatesDifference = (startDate: Date, endDate: Date): number => {
+    // Convert both dates to UTC to ensure consistency
+    const utcStartDate = Date.UTC(
+        startDate.getFullYear(),
+        startDate.getMonth(),
+        startDate.getDate()
+        );
+    const utcEndDate = Date.UTC(
+        endDate.getFullYear(),
+        endDate.getMonth(),
+        endDate.getDate()
+        );
+    // Calculate the difference in milliseconds
+    const timeDifference = utcEndDate - utcStartDate;
+    // Convert the difference from milliseconds to days
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    return daysDifference;
+};
