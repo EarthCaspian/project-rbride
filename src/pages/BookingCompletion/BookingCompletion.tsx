0bb6164aa7aae6c3125dff23ce2290205cbbebfe
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-02-14 11:28:17 +0300

    •	Added Fetch RentalId •	Added dispatch hook to add the invoice to DB

--- src/pages/BookingCompletion/BookingCompletion.tsx
+++ src/pages/BookingCompletion/BookingCompletion.tsx
@@ -1,33 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BookingStepsCard from "../../components/BookingStepsCard/BookingStepsCard";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/configureStore";
+import InvoiceService from "../../services/InvoiceService";
+import RentalService from "../../services/RentalService";
+import { InvoiceModel } from "../../models/response/Invoice.Model";
+import { RentalIdRequestModel } from "../../models/requests/RentalIdRequestModel";
+import { handleRentalIdForInvoice } from "../../store/invoiceSlice";
+import { formatStringDate } from "../../utils/formatDate";
+import { IconComponent, icons } from "../../utils/icons";
+import GoogleMapLocations from "../../utils/GoogleMapLocations";
+import InvoicePDFDocument from "../../components/InvoicePDFDocument/InvoicePDFDocument";
 
 type Props = {};
 
-const BookingCompletion = (props: Props) => {
-
+const BookingCompletion: React.FC = (props: Props) => {
+  const dispatch = useDispatch();
   const rentalState = useSelector((state: RootState) => state.rental.rental);
+  const invoiceState = useSelector((state: RootState) => state.invoice.invoice);
+  const rentalExtraState = useSelector((state: RootState) => state.rental);
+  const customer = useSelector((state: RootState) => state.customer.customer);
+
+
+  useEffect(() => {
+    FetchRentalId();
+  }, []);
+
+  //  Fetching rentalId
+  const FetchRentalId = () => {
+    const rentalIdRequestModel: RentalIdRequestModel = {
+      startDate: rentalState.startDate,
+      endDate: rentalState.endDate,
+      carId: rentalState.car.id,
+      userId: 1, //TO DO: to be updated!!!
+    };
+  
+    RentalService.getRentalId(rentalIdRequestModel)
+      .then((response) => {
+        console.log("Rental id fetched: ", response.data);
+        dispatch(handleRentalIdForInvoice([response.data]));
+
+        const invoiceModel: InvoiceModel = {
+          invoiceNo: invoiceState.invoiceNo,
+          totalPrice: invoiceState.totalPrice,
+          discountRate: invoiceState.discountRate,
+          taxRate: invoiceState.taxRate,
+          rentalId: response.data.id,
+        };
+        return InvoiceService.add(invoiceModel);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <div>
       <BookingStepsCard stepPage="StepCompletion"></BookingStepsCard>
 
-      <div className="container text-center">
-        <h1 className="success">Your reservation has been confirmed!</h1>
-        <h2 className="">Thanks for choosing our services</h2>
+      <div className="container text-center mt-5">
+        <h2 className="">Thank you for selecting our services!</h2>
         <p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="17"
-            height="17"
-            fill="currentColor"
-            className="bi bi-mailbox mb-1 me-2"
-            viewBox="0 0 16 16"
-          >
-            <path d="M4 4a3 3 0 0 0-3 3v6h6V7a3 3 0 0 0-3-3m0-1h8a4 4 0 0 1 4 4v6a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7a4 4 0 0 1 4-4m2.646 1A4 4 0 0 1 8 7v6h7V7a3 3 0 0 0-3-3z" />
-            <path d="M11.793 8.5H9v-1h5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.354-.146zM5 7c0 .552-.448 0-1 0s-1 .552-1 0a1 1 0 0 1 2 0" />
-          </svg>
+          <IconComponent iconName="Mailbox" />
           Booking details have been sent to your email address. Please check
           your mailbox.
         </p>
@@ -45,90 +79,89 @@ const BookingCompletion = (props: Props) => {
             <div className="col-md-8">
               <div className="card-body">
                 <h2 className="card-title">Reservation Overview</h2>
-                <p className="card-text">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-geo-alt mb-2 me-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                  </svg>
-                  <strong>Pick up: </strong>İstanbul Sabiha Gökçen Havalimanı / {rentalState.startDate}
-                </p>
-                <p className="card-text">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-geo-alt mb-2 me-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                  </svg>
-                  <strong>Return: </strong>İstanbul Sabiha Gökçen Havalimanı / {rentalState.endDate}
-                </p>
+
                 <h3 className="card-text">
-                  <strong> {rentalState.car.model.brand.name + " " + rentalState.car.model.name} </strong>
+                  <strong>
+                    {" "}
+                    {rentalState.car.model.brand.name +
+                      " " +
+                      rentalState.car.model.name}{" "}
+                  </strong>
                 </h3>
                 <p>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-car-front"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0m10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17s2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276" />
-                    <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.8.8 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155s4.037-.084 5.592-.155A1.48 1.48 0 0 0 15 9.611v-.413q0-.148-.03-.294l-.335-1.68a.8.8 0 0 0-.43-.563 1.8 1.8 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3z" />
-                  </svg>
-                  
-                    Manuel   
-                  
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-fuel-pump ms-3 me-1 mb-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M3 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5z" />
-                    <path d="M1 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v8a2 2 0 0 1 2 2v.5a.5.5 0 0 0 1 0V8h-.5a.5.5 0 0 1-.5-.5V4.375a.5.5 0 0 1 .5-.5h1.495c-.011-.476-.053-.894-.201-1.222a.97.97 0 0 0-.394-.458c-.184-.11-.464-.195-.9-.195a.5.5 0 0 1 0-1q.846-.002 1.412.336c.383.228.634.551.794.907.295.655.294 1.465.294 2.081v3.175a.5.5 0 0 1-.5.501H15v4.5a1.5 1.5 0 0 1-3 0V12a1 1 0 0 0-1-1v4h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1zm9 0a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13h8z" />
-                  </svg>
-                  
-                    Benzin
-                  
-
+                  <IconComponent iconName="CarFromFront" />
+                  Manuel
+                  {"   "}
+                  <IconComponent iconName="FuelPump" />
+                  Benzin - Our cars come with a full tank.
                 </p>
 
+                <p className="card-text mb-1">We kindly remind you! </p>
 
-                <p className="card-text">
-                  <small className="text-body-secondary">
-                    Our car is provided with a full tank.
+                <p className="card-text text-body-secondary mb-1">
+                  <small>
+                    Any changes to the rental period must be communicated and
+                    agreed upon by both parties.Renters must possess a valid
+                    driver's license and meet the minimum age requirement as per
+                    local regulations.
                   </small>
                 </p>
-                <p className="card-text">
-                  
-                  <small className="text-body-secondary">
-                    If you have any questions about the book details, feel free
-                    to get in touch with us!
+                <p className="card-text text-body-secondary mb-1">
+                  <small>
+                    The rented vehicle is for personal use only and may not be
+                    used for commercial purposes unless specified and agreed
+                    upon.
                   </small>
                 </p>
 
-
-
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Should you have any inquiries regarding the booking details,
+                    please don't hesitate to contact us!
+                  </small>
+                </p>
+                <h6>
+                  Contact with us
+                  <IconComponent iconName="Telephone" />
+                  0212 444 55 88
+                </h6>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="container mb-4">
+        <div className="row">
+          <div className="col-4 mx-auto">
+            <div className="">
+              <h4>Pick-up Information</h4>
+              <p>
+                <IconComponent iconName="Clock" />
+                {formatStringDate(rentalState.startDate)}
+              </p>
+              <GoogleMapLocations locationName="İstanbul Airport" />
+            </div>
+          </div>
+          <div className="col-4 mx-auto">
+            <div className="">
+              <h4>Drop-off Information</h4>
+              <p>
+                <IconComponent iconName="Clock" />
+                {formatStringDate(rentalState.endDate)}
+              </p>
+              <GoogleMapLocations locationName="İstanbul Airport" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <InvoicePDFDocument
+        customer={customer}
+        invoice={invoiceState}
+        rental={rentalState}
+        rentalExtras={rentalExtraState}
+      />
     </div>
   );
 };
