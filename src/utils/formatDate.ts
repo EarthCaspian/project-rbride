332d83c43d593f1525d673c4a38d5626f4e92e72
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-02-14 11:50:22 +0300

    Added new functions for date formating

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -5,3 +5,50 @@ export const formatLocalDateToYYYYMMDD = (date: string) : string => {
     return formattedDate;
 }
 
+
+export const formatStringDate = (date: string): string => {
+    // Parse the input date string
+    const parsedDate = new Date(date);
+
+    // Get the day of the week (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const dayOfWeek = days[parsedDate.getDay()];
+
+    // Get the day, month, and year
+    const day = parsedDate.getDate().toString().padStart(2, "0");
+    const month = (parsedDate.getMonth() + 1).toString().padStart(2, "0");
+    const year = parsedDate.getFullYear().toString();
+
+    // Return the formatted date string
+    return `${dayOfWeek} ${day}-${month}-${year}`;
+  };
+
+  export const formatDateToStringDate = (date: Date): string => {
+    // Get the day of the week (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const dayOfWeek = days[date.getDay()];
+
+    // Get the day, month, and year
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString();
+
+    // Return the formatted date string
+    return `${dayOfWeek} ${day}-${month}-${year}`;
+};
