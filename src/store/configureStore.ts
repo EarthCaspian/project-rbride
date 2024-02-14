8deb89078ce6a4b7f0a5abd1e6e06ad33983bf46
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-02-14 11:42:52 +0300

    Added new Reducers

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,15 +1,20 @@
-import { combineReducers, configureStore} from "@reduxjs/toolkit";
-import { cartReducer} from "./cartSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { cartReducer } from "./cartSlice";
 import { rentalReducer } from "./rentalSlice";
 import { loadingReducer } from "./loadingSlice";
 import { loginReducer } from "./loginSlice";
-
+import { customerReducer } from "./customerSlice";
+import { invoiceReducer } from "./invoiceSlice";
+import { bookingReducer, bookingSlice } from "./bookingSlice";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
   rental: rentalReducer,
   loading: loadingReducer,
-  login: loginReducer
+  login: loginReducer,
+  customer: customerReducer,
+  invoice: invoiceReducer,
+  booking: bookingReducer,
 });
 
 export const store = configureStore({
@@ -17,4 +22,4 @@ export const store = configureStore({
 });
 
 export default store;
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer>;
