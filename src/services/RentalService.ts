c3fd442c965eb927da3354638e1cc1b441c3c73f
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-02-14 11:59:38 +0300

    Added getRentalId function

--- src/services/RentalService.ts
+++ src/services/RentalService.ts
@@ -1,19 +1,39 @@
+import { AddRentalRequestModel } from "./../models/requests/AddRentalRequestModel";
 import axios from "axios";
-import { AddRentalRequestModel } from "../models/response/RentalModel";
+import { RentalResponseModel } from "../models/response/RentalResponseModel";
+import { GetRentalIdModel } from "../models/response/GetRentalIdModel";
+import { RentalIdRequestModel } from "../models/requests/RentalIdRequestModel";
 
-const API_URL="http://localhost:8080/api/rentals";
+const API_URL = "http://localhost:8080/api/rentals";
 
 class RentalService {
-    //Function to send a request to the backend for adding a rental entry to the database.
-    add(request : AddRentalRequestModel) {
-        axios.post(`${API_URL}/add`, request)
-        .then(response => {
-            console.log(response + ": The rental has been successfully added to the database.");
-        })
-        .catch(error => {
-            console.log(error + ": Invalid parameters or malformed data.");
-        })
-    }
+  //Function to send a request to the backend for adding a rental entry to the database.
+  add(request: AddRentalRequestModel) {
+    axios
+      .post<AddRentalRequestModel>(`${API_URL}/add`, request)
+      .then((response) => {
+        console.log(
+          response + ": The rental has been successfully added to the database."
+        );
+      })
+      .catch((error) => {
+        console.log(error + ": Invalid parameters or malformed data.");
+      });
+  }
+  getById(id: number) {
+    return axios.get<RentalResponseModel>(`${API_URL}/getById/${id}`);
+  }
+
+  getRentalId(requestModel: RentalIdRequestModel) {
+    return axios.get<GetRentalIdModel>(`${API_URL}/getRentalId`, {
+      params: {
+        startDate: requestModel.startDate,
+        endDate: requestModel.endDate,
+        carId: requestModel.carId,
+        userId: requestModel.userId,
+      },
+    });
+  }
 }
 
 export default new RentalService();
