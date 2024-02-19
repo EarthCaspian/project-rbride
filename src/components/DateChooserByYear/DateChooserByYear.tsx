9c57bc0256765f39da2c5226e57a9f5cffc5b787
senemyilmazz <senemy97@gmail.com>
2024-02-19 10:48:59 +0300

    Changed method of handling the value and the props model.

--- src/components/DateChooserByYear/DateChooserByYear.tsx
+++ src/components/DateChooserByYear/DateChooserByYear.tsx
@@ -1,24 +1,21 @@
+import { FieldProps, useField } from "formik";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-interface DateChooserProps {
-  onDateChange: (date: Date) => void;
-  selectedDate: Date | null;
+interface DateChooserFieldProps extends FieldProps{
+  name ?: string | null;
   minDate: Date | null;
   maxDate: Date | null;
 }
 
-const DateChooser: React.FC<DateChooserProps> = ({
-  onDateChange,
-  minDate,
-  maxDate,
-}) => {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+const DateChooserField: React.FC<DateChooserFieldProps> = (props : DateChooserFieldProps) => {
+  const [field, state, {setValue}] = useField(props.field.name);
+  const [selectedDate, setSelectedDate] = useState<Date | null> (null);
 
-  const handleDateChange = (date: Date) => {
-    setSelectedDate(date);
-    onDateChange(date);
+  const handleDateChange = (value : Date | null) => {
+    setSelectedDate(value);
+    setValue(value?.toJSON());
   };
 
   return (
@@ -30,11 +27,11 @@ const DateChooser: React.FC<DateChooserProps> = ({
       scrollableYearDropdown
       yearDropdownItemNumber={100}
       placeholderText="Select a date"
-      minDate={minDate}
-      maxDate={maxDate}
+      minDate={props.minDate}
+      maxDate={props.maxDate}
       className="border border-secondary border-1 border-opacity-25"
     />
   );
 };
 
-export default DateChooser;
+export default DateChooserField;
