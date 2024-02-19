669f7da48b9bf743caf6f37cc51a26621d4f6585
senemyilmazz <senemy97@gmail.com>
2024-02-19 10:46:50 +0300

    Retrieving userId information, and now the onSubmit event of the FormCard is triggered by the button here. .

--- src/pages/BookNow/BookNow.tsx
+++ src/pages/BookNow/BookNow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import { useDispatch, useSelector } from "react-redux";
 import CustomerService from "../../services/CustomerService";
@@ -16,69 +16,88 @@ import { setStepLevel } from "../../store/stepsSlice";
 type Props = {};
 
 const BookNow = (props: Props) => {
-  const [rentalId, setRentalId] = useState<number>(0);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const CustomerState = useSelector(
-    (state: RootState) => state.customer.customer
-  );
+  const CustomerState = useSelector((state: RootState) => state.customer);
   const RentalState = useSelector((state: RootState) => state.rental.rental);
+  const UserState = useSelector((state : RootState) => state.user);
   const dispatch = useDispatch();
   //const IsBookedState = useSelector((state: RootState) => state.booking.isBookedCompleted);
 
   const togglePopup = () => {
-    console.log("Toggle popup is working"); //Delete!!!!!
+    console.log("Toggle popup is working"); //Delete!!!!! 
   };
 
+  useEffect(() => {
+    CompleteBooking();
+  }, [CustomerState.isValid]);
+
   const CompleteBooking = async () => {
-    console.log("Completing booking button works: "); //Delete!!!!!
-    const customerModel: CustomerModel = {
-      firstName: CustomerState.firstName,
-      lastName: CustomerState.lastName,
-      birthdate: CustomerState.birthdate,
-      internationalId: CustomerState.internationalId,
-      licenceIssueDate: CustomerState.licenceIssueDate,
-      userId: 1,
-    };
+     if (CustomerState.isValid) {
 
-    const rentalModel: AddRentalRequestModel = {
-      startDate: new Date(RentalState.startDate),
-      endDate: new Date(RentalState.endDate),
-      carId: RentalState.car.id,
-      userId: 1,
-    };
+      const customerModel: CustomerModel = {
+        firstName: CustomerState.customer.firstName,
+        lastName: CustomerState.customer.lastName,
+        birthdate:new Date(CustomerState.customer.birthdate),
+        internationalId: CustomerState.customer.internationalId,
+        licenceIssueDate: new Date(CustomerState.customer.licenceIssueDate),
+        userId: UserState.userId,
+      };  
 
-    dispatch(setStepLevel(3));
-    
-    try {
-      await Promise.all([
-        RentalService.add(rentalModel),
-        CustomerService.add(customerModel),
-      ]);
-      setIsPopupOpen(true);
-    } catch (error) {
-      console.error("Error while adding customer or rental", error);
+      const rentalModel: AddRentalRequestModel = {
+        startDate: new Date(RentalState.startDate),
+        endDate: new Date(RentalState.endDate),
+        carId: RentalState.car.id,
+        userId: UserState.userId,
+      };
+
+      dispatch(setStepLevel(3));
+
+      try {
+        await Promise.all([
+          RentalService.add(rentalModel),
+          CustomerService.add(customerModel),
+        ]);
+        setIsPopupOpen(true);
+      } catch (error) {
+          console.error("Error while adding customer or rental", error);
+      }
     }
   };
   return (
     <div id="bookNow">
+
+      {/* BOOKING STEPS CARD */}
       <div className="container-fluid text-center border-bottom py-4">
         <BookingStepsCard stepPage="BookNow"></BookingStepsCard>
         <h1>Check & Complete Booking</h1>
       </div>
+
       <div className="container mt-5">
         <div className="row">
+
+          {/* BOOKING DETAILS CARD */}
           <div className="col-4">
             <BookingDetailsCard></BookingDetailsCard>
           </div>
           <div className="col-8">
+            {/* CUSTOMER FORM CARD */}
             <CustomerFormCard />
+            {/* PAYMENT DETAILS CARD */}
             <PaymentDetailsCard />
+
+            {/* COMPLETE BOOKING BUTTON */}
             <div className="col-12 fw-bold mb-2">
-              <button className="btn btn-success" onClick={CompleteBooking}>
+              <button id="complete-booking" 
+                  className="btn btn-success" 
+                  form="customer-form" 
+                  type="submit"
+                  onClick={CompleteBooking}
+              >
                 Complete Booking
               </button>
+
               {isPopupOpen && (
-                <Popup onClose={togglePopup} rentalId={rentalId} />
+                <Popup onClose={togglePopup} />
               )}
             </div>
           </div>
