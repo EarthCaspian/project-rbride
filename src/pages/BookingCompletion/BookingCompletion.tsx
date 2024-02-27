9ea398c0a70d861764dc5d5692ed0f6f8705bd58
senemyilmazz <senemy97@gmail.com>
2024-02-27 23:04:25 +0300

    Fixed popstate event interception bug.

--- src/pages/BookingCompletion/BookingCompletion.tsx
+++ src/pages/BookingCompletion/BookingCompletion.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import BookingStepsCard from "../../components/BookingStepsCard/BookingStepsCard";
 import { useDispatch, useSelector } from "react-redux";
 import { IconComponent } from "../../utils/icons";
