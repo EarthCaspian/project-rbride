3d64e4d69189b8efe8751ded7b41e559ab433e2f
EarthCaspian <hazar.a@outlook.com>
2024-02-27 11:56:37 +0300

    promise structure moved into userService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,17 @@
 import axiosInstance from '../utils/interceptors/axiosInterceptors'; 
+import RoleService from './RoleService';
 
 class UserService {
   getRolesByUserId(userId: number) {
-    return axiosInstance.get(`users/${userId}/roles`);
+    return axiosInstance.get(`users/${userId}/roles`)
+    .then(response => {
+      const roleName = response.data[0].name;
+      const isAdmin = roleName === 'admin';
+      RoleService.setRole(isAdmin ? 'admin' : 'user');
+    })
+    .catch(error => {
+      console.log(error);
+    });;
   }
 }
 
